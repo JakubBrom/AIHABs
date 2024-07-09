31f478890977bb57194851a80efc7ca509881d14
JakubBrom <jbrom@fzt.jcu.cz>
2024-07-09 10:34:00 +0200

    Redefining example model

--- AI_model_example.py
+++ AI_model_example.py
@@ -1,46 +1,44 @@
 # Imports
 import time
 import pickle
+import joblib
+import numpy as np
 
 
 # 1. Define AI model for WQ parameter prediction: e.g. ChlA. The model can be more complex (e.g. Class). The parameters need to be defined in this way
-def AI_model_example(B01, B02, B03, B04, B05, B06, B07, B08, B8A, B09, B11, B12):
-    """
-    Example function for e.g. ChlA AI prediction model. The input parameters are bands from Sentinel-2 image (L2A level) or values for particular pixel.
-    The values are INT in default (e.i. original values from the image). The numpy arrays can ce used.
+class AI_model_example():
+    def __init__(self, model_path):
+        self.model = joblib.load(model_path)
 
-    :param B01: Surface spectral reflectance (0 to 1) for particular pixel or for some area of the image (float)
-    :param B02: ...
-    :param B03: ...
-    :param B04: ...
-    :param B05: ...
-    :param B06: ...
-    :param B07: ...
-    :param B08: ...
-    :param B8A: ...
-    :param B09: ...
-    :param B11: ...
-    :param B12: ...
-    :return: Value or Numpy array
-    """
+    def preprocess(self, input_data):
+        # Do some work with data here
+        processed_data = input_data
 
-    # imports --> define imports here
-    from sklearn.linear_model import LinearRegression
+        return processed_data
 
-    # Do some work, e.g.
-    prediction = B05 / B04
-    time.sleep(5)
+    def predict(self, input_data):
+        # Predict the data using the model
+        processed_data = self.preprocess(input_data)
+        predicted_data = self.model.predict(processed_data)
 
-    return prediction
+        # Do some postprocessing here
+        postprocessed_data = predicted_data
 
+        return postprocessed_data
 
 # 2. Serialize the model to pickle file. The file will be used for predictions afterwards.
 # We can store various models in the database (e.g. for particular WQ parameter and water reservoir id).
+model_path = 'model.joblib'
+model_instance = AI_model_example(model_path)
 with open('AI_model_example.pkl', 'wb') as f:
-    pickle.dump(AI_model_example, f)
+    pickle.dump(model_instance, f)
 
 # 3. Load the model from pickle file - will be done in the calculation script. In this place the pickle file is a blackbox.
 with open('AI_model_example.pkl', 'rb') as f:
     AI_model_example = pickle.load(f)
 
-chla_conc = AI_model_example(B01, B02, B03, B04, B05, B06, B07, B08, B8A, B09, B11, B12)
+input_data = [B01, B02, B03, B04, B05, B06, B07, B08, B8A, B09, B11, B12]   # Surface spectral reflectances (0 to 1)
+        # for particular pixel or for some area of the image for particular bands (float)
+
+# 4. Run the model
+chla_conc = AI_model_example.predict(input_data)
