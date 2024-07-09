5593da8dcb750aa10b7a216f373f3ff03196b95a
JakubBrom <jbrom@fzt.jcu.cz>
2024-07-09 10:30:15 +0200

    Redefining example model

---
+++ AIHABs.py
@@ -0,0 +1,18 @@
+# Imports
+
+
+class AIHABs:
+    def __init__(self):
+        pass
+
+    def run_analyse(self):
+
+        # get Sentinel-2 data --> OpenEO
+        # calculate WQ features --> new AI models
+
+        # get meteodata
+        # get predicted meteodata
+        # make dataset for AI time series analysis
+        # run AI time series analysis
+        pass
+
